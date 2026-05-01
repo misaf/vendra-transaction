1d3fc33baa61923fe67903f7daa986203d48081d
Misaf <misaf.1990@gmail.com>
2026-05-01 09:13:44 +0330

    closed #10: Remove registerMediaCollections, move logic to registerMediaConversions

--- src/Models/TransactionGateway.php
+++ src/Models/TransactionGateway.php
@@ -92,7 +92,7 @@ final class TransactionGateway extends Model implements HasMedia, Sortable
         return $this->media();
     }
 
-    public function registerMediaCollections(): void
+    public function registerMediaConversions(?Media $media = null): void
     {
         $this->addMediaConversion('thumb-table')
             ->width(48)
@@ -114,8 +114,6 @@ final class TransactionGateway extends Model implements HasMedia, Sortable
             ->width(1200)
             ->format('webp');
     }
-
-    public function registerMediaConversions(?Media $media = null): void {}
 
     public function getSlugOptions(): SlugOptions
     {
