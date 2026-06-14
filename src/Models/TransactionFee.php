8c3e516b27069f90ba8d1b0a86fc6f456b0d7009
Misaf <misaf.1990@gmail.com>
2026-06-14 13:17:22 +0330

    Use factory attribute on TransactionFee model

--- src/Models/TransactionFee.php
+++ src/Models/TransactionFee.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Misaf\VendraTransaction\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
@@ -21,6 +22,7 @@ use Spatie\Activitylog\Traits\LogsActivity;
  * @property Carbon $updated_at
  */
 #[Fillable(['transaction_id', 'amount'])]
+#[UseFactory(TransactionFeeFactory::class)]
 final class TransactionFee extends Model
 {
     use BelongsToTransaction;
