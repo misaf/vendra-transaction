be7aadd37e66559c1efde01cf8a93a6caa514a4c
Misaf <misaf.1990@gmail.com>
2026-06-14 13:17:12 +0330

    Use UseModel attribute in TransactionMetadataFactory

--- database/factories/TransactionMetadataFactory.php
+++ database/factories/TransactionMetadataFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Misaf\VendraTransaction\Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Attributes\UseModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Misaf\VendraTransaction\Models\Transaction;
 use Misaf\VendraTransaction\Models\TransactionMetadata;
@@ -11,13 +12,9 @@ use Misaf\VendraTransaction\Models\TransactionMetadata;
 /**
  * @extends Factory<TransactionMetadata>
  */
+#[UseModel(TransactionMetadata::class)]
 final class TransactionMetadataFactory extends Factory
 {
-    /**
-     * @var class-string<TransactionMetadata>
-     */
-    protected $model = TransactionMetadata::class;
-
     /**
      * @return array<string, mixed>
      */
