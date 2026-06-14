df9a786cf87159c4a29214b025346edb82e022cc
Misaf <misaf.1990@gmail.com>
2026-06-14 13:17:23 +0330

    Use factory attribute on TransactionTransfer model

--- src/Models/TransactionTransfer.php
+++ src/Models/TransactionTransfer.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Misaf\VendraTransaction\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 use Misaf\VendraActivityLog\Concerns\HasDefaultActivityLogOptions;
+use Misaf\VendraTransaction\Database\Factories\TransactionTransferFactory;
 use Misaf\VendraTransaction\Traits\BelongsToTransaction;
-use Misaf\VendraUser\Database\Factories\UserFactory;
 use Misaf\VendraUser\Traits\BelongsToUser;
 use Spatie\Activitylog\Traits\LogsActivity;
 
@@ -22,12 +23,13 @@ use Spatie\Activitylog\Traits\LogsActivity;
  * @property Carbon $updated_at
  */
 #[Fillable(['transaction_id', 'user_id'])]
+#[UseFactory(TransactionTransferFactory::class)]
 final class TransactionTransfer extends Model
 {
     use BelongsToTransaction;
     use BelongsToUser;
     use HasDefaultActivityLogOptions;
-    /** @use HasFactory<UserFactory> */
+    /** @use HasFactory<TransactionTransferFactory> */
     use HasFactory;
     use LogsActivity;
 
