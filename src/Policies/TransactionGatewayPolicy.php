8de2c646e6219c9b17bf8243ee47253d77b20114
Misaf <misaf.1990@gmail.com>
2026-07-06 13:06:35 +0330

    Clean up module autoload and policies

--- src/Policies/TransactionGatewayPolicy.php
+++ src/Policies/TransactionGatewayPolicy.php
@@ -5,64 +5,64 @@ declare(strict_types=1);
 namespace Misaf\VendraTransaction\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraTransaction\Models\TransactionGateway;
-use Misaf\VendraUser\Models\User;
 
 final class TransactionGatewayPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
         return $user->can('create-transaction-gateway');
     }
 
-    public function delete(User $user, TransactionGateway $transactionGateway): bool
+    public function delete(Authorizable $user, TransactionGateway $transactionGateway): bool
     {
         return $user->can('delete-transaction-gateway');
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
         return $user->can('delete-any-transaction-gateway');
     }
 
-    public function forceDelete(User $user, TransactionGateway $transactionGateway): bool
+    public function forceDelete(Authorizable $user, TransactionGateway $transactionGateway): bool
     {
         return $user->can('force-delete-transaction-gateway');
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
         return $user->can('force-delete-any-transaction-gateway');
     }
 
-    public function replicate(User $user, TransactionGateway $transactionGateway): bool
+    public function replicate(Authorizable $user, TransactionGateway $transactionGateway): bool
     {
         return $user->can('replicate-transaction-gateway');
     }
 
-    public function restore(User $user, TransactionGateway $transactionGateway): bool
+    public function restore(Authorizable $user, TransactionGateway $transactionGateway): bool
     {
         return $user->can('restore-transaction-gateway');
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
         return $user->can('restore-any-transaction-gateway');
     }
 
-    public function update(User $user, TransactionGateway $transactionGateway): bool
+    public function update(Authorizable $user, TransactionGateway $transactionGateway): bool
     {
         return $user->can('update-transaction-gateway');
     }
 
-    public function view(User $user, TransactionGateway $transactionGateway): bool
+    public function view(Authorizable $user, TransactionGateway $transactionGateway): bool
     {
         return $user->can('view-transaction-gateway');
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
         return $user->can('view-any-transaction-gateway');
     }
--- src/Policies/TransactionPolicy.php
+++ src/Policies/TransactionPolicy.php
@@ -5,64 +5,64 @@ declare(strict_types=1);
 namespace Misaf\VendraTransaction\Policies;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Misaf\VendraTransaction\Models\Transaction;
-use Misaf\VendraUser\Models\User;
 
 final class TransactionPolicy
 {
     use HandlesAuthorization;
 
-    public function create(User $user): bool
+    public function create(Authorizable $user): bool
     {
         return $user->can('create-transaction');
     }
 
-    public function delete(User $user, Transaction $transaction): bool
+    public function delete(Authorizable $user, Transaction $transaction): bool
     {
         return $user->can('delete-transaction');
     }
 
-    public function deleteAny(User $user): bool
+    public function deleteAny(Authorizable $user): bool
     {
         return $user->can('delete-any-transaction');
     }
 
-    public function forceDelete(User $user, Transaction $transaction): bool
+    public function forceDelete(Authorizable $user, Transaction $transaction): bool
     {
         return $user->can('force-delete-transaction');
     }
 
-    public function forceDeleteAny(User $user): bool
+    public function forceDeleteAny(Authorizable $user): bool
     {
         return $user->can('force-delete-any-transaction');
     }
 
-    public function replicate(User $user, Transaction $transaction): bool
+    public function replicate(Authorizable $user, Transaction $transaction): bool
     {
         return $user->can('replicate-transaction');
     }
 
-    public function restore(User $user, Transaction $transaction): bool
+    public function restore(Authorizable $user, Transaction $transaction): bool
     {
         return $user->can('restore-transaction');
     }
 
-    public function restoreAny(User $user): bool
+    public function restoreAny(Authorizable $user): bool
     {
         return $user->can('restore-any-transaction');
     }
 
-    public function update(User $user, Transaction $transaction): bool
+    public function update(Authorizable $user, Transaction $transaction): bool
     {
         return $user->can('update-transaction');
     }
 
-    public function view(User $user, Transaction $transaction): bool
+    public function view(Authorizable $user, Transaction $transaction): bool
     {
         return $user->can('view-transaction');
     }
 
-    public function viewAny(User $user): bool
+    public function viewAny(Authorizable $user): bool
     {
         return $user->can('view-any-transaction');
     }
